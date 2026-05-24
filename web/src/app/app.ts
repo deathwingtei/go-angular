7622622c4b65a4d23d4805b8d01406101e2a2347
KP <p.kittichet@gmail.com>
2026-05-24 17:23:07 +0700

    update structure

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { Header } from './core/layout/header/header';
+import { Footer } from './core/layout/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, CommonModule, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
